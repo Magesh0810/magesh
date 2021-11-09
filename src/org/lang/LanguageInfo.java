7d105451ffa0af997d1726d55f259ff3ff7048d1
magesh0810 <maadymagesh01@gmail.com>
2021-11-09 16:52:25 +0530

    task1

---
+++ src/org/lang/LanguageInfo.java
@@ -0,0 +1,16 @@
+package org.lang;
+
+public class LanguageInfo {
+	public void tamilLanguage () {
+		System.out.println("Info in Tamil");
+		
+	}
+	public void englishLanguage() {
+		System.out.println("Infoin english");
+	}
+	public void hindiLanguage() {
+		System.out.println("info in hindi");
+	}
+	
+	
+}             
---
+++ src/org/lang/StateDetails.java
@@ -0,0 +1,22 @@
+package org.lang;
+
+public class StateDetails {
+	public void southIndia() {
+		System.out.println("state is southindia ");
+	}
+	public void northIndia() {
+		System.out.println("state is northindia");
+	}
+	public static void main(String[] args) {
+		StateDetails q5 = new StateDetails();
+		q5.southIndia();
+		q5.northIndia();
+		
+		LanguageInfo t5 = new  LanguageInfo();
+		t5.englishLanguage();
+		t5.englishLanguage();
+		t5.hindiLanguage();
+
+	}
+
+}
